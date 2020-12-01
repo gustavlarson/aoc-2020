79d8cf28279e6184cc05e7147b214de888a513a8
Gustav Larson <gustav.larson@gmail.com>
2020-12-01 10:13:26 +0100

    Basic test, handle only two numbers

--- src/main/java/com/gustavlarson/aoc/day/Day01.java
+++ src/main/java/com/gustavlarson/aoc/day/Day01.java
@@ -3,10 +3,14 @@ package com.gustavlarson.aoc.day;
 import com.gustavlarson.aoc.Day;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day01 implements Day {
     @Override
     public String solve(final List<String> input) {
-        return null;
+        final List<Integer> expenses = input.parallelStream().map(Integer::parseInt).collect(Collectors.toList());
+
+        final Integer result = expenses.stream().reduce(1, (a, b) -> a * b);
+        return "" + result;
     }
 }
