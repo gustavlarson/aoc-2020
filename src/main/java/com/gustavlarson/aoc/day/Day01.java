52b383a6903560d0d77ea1ac704d673c400c942e
Gustav Larson <gustav.larson@gmail.com>
2020-12-01 11:13:30 +0100

    Part 2 passing

--- src/main/java/com/gustavlarson/aoc/day/Day01.java
+++ src/main/java/com/gustavlarson/aoc/day/Day01.java
@@ -9,13 +9,20 @@ public class Day01 implements Day {
     private static int solver(final List<String> input, final int count) {
         final List<Integer> expenses = input.parallelStream().map(Integer::parseInt).collect(Collectors.toList());
 
-        // Find the two elements that adds to 2020
+        // Find the n (count) elements that adds to 2020
         final List<Integer> result = expenses.parallelStream().filter(
-                outer -> expenses.parallelStream().anyMatch(inner -> inner + outer == 2020)
+                value -> matcher(expenses, value, count)
         ).collect(Collectors.toList());
 
         // Multiply the elements
         return result.stream().reduce(1, (a, b) -> a * b);
+    }
+
+    private static boolean matcher(final List<Integer> input, final int value, final int count) {
+        if (count == 1) {
+            return value == 2020;
+        }
+        return input.parallelStream().anyMatch(v -> matcher(input, v + value, count - 1));
     }
 
     @Override
