5030411ed2828f49cf8aa9ef37acad1a574bc6b9
Gustav Larson <gustav.larson@gmail.com>
2020-12-02 13:38:19 +0100

    Prepare Day03 solution

---
+++ src/test/java/com/gustavlarson/aoc/day/Day03Test.java
@@ -0,0 +1,19 @@
+package com.gustavlarson.aoc.day;
+
+import com.gustavlarson.aoc.Day;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day03Test {
+    @Test
+    public void testPart101() {
+        final Day day = new Day01();
+        final List<String> input = List.of("line1", "line2");
+        final String result = day.solvePart1(input);
+        assertEquals("", result);
+    }
+
+}
