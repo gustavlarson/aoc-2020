b4e0dbee8288886d30847a0fc98fb7c895f77d47
Gustav Larson <gustav.larson@gmail.com>
2020-12-02 07:56:14 +0100

    Add test and input

---
+++ src/test/java/com/gustavlarson/aoc/day/Day02Test.java
@@ -0,0 +1,12 @@
+package com.gustavlarson.aoc.day;
+
+import com.gustavlarson.aoc.Day;
+import org.junit.jupiter.api.Test;
+
+public class Day02Test {
+    @Test
+    public void test01() {
+        final Day day = new Day02();
+    }
+
+}
