300abbd6294ecdac42e3e94eef011b1a9fb05292
Gustav Larson <gustav.larson@gmail.com>
2020-12-02 08:31:18 +0100

    Cleanup Day02

--- src/main/java/com/gustavlarson/aoc/day/Day02.java
+++ src/main/java/com/gustavlarson/aoc/day/Day02.java
@@ -7,45 +7,50 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day02 implements Day {
-    Pattern pattern = Pattern.compile("(?<min>\\d+)-(?<max>\\d+) (?<letter>[a-zA-Z]): (?<password>\\w+)");
+    private static final Pattern pattern = Pattern.compile("(?<min>\\d+)-(?<max>\\d+) (?<letter>[a-zA-Z]): (?<password>\\w+)");
 
-    private boolean isValidPart1(final String line) {
+    private static Matcher getMatcher(final String line) {
         final Matcher matcher = pattern.matcher(line);
         if (!matcher.find()) {
             throw new IllegalArgumentException();
         }
+        return matcher;
+    }
+
+    private static boolean isValidPart1(final String line) {
+        final Matcher matcher = getMatcher(line);
         final int minCount = Integer.parseInt(matcher.group("min"));
         final int maxCount = Integer.parseInt(matcher.group("max"));
-        final String letter = matcher.group("letter");
+        final char letter = matcher.group("letter").charAt(0);
         final String password = matcher.group("password");
 
-        final long count = password.chars().parallel().filter(c -> c == letter.charAt(0)).count();
+        final long count = password.chars().parallel().filter(c -> c == letter).count();
 
         return count >= minCount && count <= maxCount;
     }
 
     @Override
     public String solvePart1(final List<String> input) {
-        final long validPasswords = input.parallelStream().filter(this::isValidPart1).count();
+        final long validPasswords = input.parallelStream().filter(Day02::isValidPart1).count();
         return "" + validPasswords;
     }
 
-    private boolean isValidPart2(final String line) {
-        final Matcher matcher = pattern.matcher(line);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException();
-        }
-        final int minCount = Integer.parseInt(matcher.group("min"));
-        final int maxCount = Integer.parseInt(matcher.group("max"));
-        final String letter = matcher.group("letter");
+    private static boolean isValidPart2(final String line) {
+        final Matcher matcher = getMatcher(line);
+        final int firstPosition = Integer.parseInt(matcher.group("min"));
+        final int secondPosition = Integer.parseInt(matcher.group("max"));
+        final char letter = matcher.group("letter").charAt(0);
         final String password = matcher.group("password");
 
-        return (password.charAt(minCount - 1) == letter.charAt(0) ^ password.charAt(maxCount - 1) == letter.charAt(0));
+        final boolean firstMatches = password.charAt(firstPosition - 1) == letter;
+        final boolean secondMatches = password.charAt(secondPosition - 1) == letter;
+
+        return (firstMatches ^ secondMatches);
     }
 
     @Override
     public String solvePart2(final List<String> input) {
-        final long validPasswords = input.parallelStream().filter(this::isValidPart2).count();
+        final long validPasswords = input.parallelStream().filter(Day02::isValidPart2).count();
         return "" + validPasswords;
     }
 }
