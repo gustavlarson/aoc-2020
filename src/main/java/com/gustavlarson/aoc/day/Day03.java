117f85e64d2f47dc412d053acdb57958f05e0f18
Gustav Larson <gustav.larson@gmail.com>
2020-12-03 07:55:06 +0100

    Solve part 1

--- src/main/java/com/gustavlarson/aoc/day/Day03.java
+++ src/main/java/com/gustavlarson/aoc/day/Day03.java
@@ -3,12 +3,16 @@ package com.gustavlarson.aoc.day;
 import com.gustavlarson.aoc.Day;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Day03 implements Day {
 
     @Override
     public String solvePart1(final List<String> input) {
-        return null;
+        final int patternLength = input.get(0).length();
+        return "" + IntStream.range(0, input.size()).filter(i -> input.get(i).charAt((i * 3) % patternLength) == '#'
+        ).count();
+        //return "0";
     }
 
     @Override
--- src/test/java/com/gustavlarson/aoc/day/Day03Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day03Test.java
@@ -10,10 +10,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day03Test {
     @Test
     public void testPart101() {
-        final Day day = new Day01();
-        final List<String> input = List.of("line1", "line2");
+        final Day day = new Day03();
+        final List<String> input = List.of("..##.......");
         final String result = day.solvePart1(input);
-        assertEquals("", result);
+        assertEquals("0", result);
     }
 
+    @Test
+    public void testPart102() {
+        final Day day = new Day03();
+        final List<String> input = List.of("..##.......", "#...#...#..");
+        final String result = day.solvePart1(input);
+        assertEquals("0", result);
+    }
+
+    @Test
+    public void testPart103() {
+        final Day day = new Day03();
+        final List<String> input = List.of("..##.......", "#...#...#..", ".#....#..#.");
+        final String result = day.solvePart1(input);
+        assertEquals("1", result);
+    }
 }
