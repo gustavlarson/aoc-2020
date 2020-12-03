55a0da8ec5f47f0b6c723d3ecb2914dda66101dd
Gustav Larson <gustav.larson@gmail.com>
2020-12-03 08:54:09 +0100

    Refactor

--- src/main/java/com/gustavlarson/aoc/day/Day03.java
+++ src/main/java/com/gustavlarson/aoc/day/Day03.java
@@ -2,6 +2,7 @@ package com.gustavlarson.aoc.day;
 
 import com.gustavlarson.aoc.Day;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -10,31 +11,30 @@ public class Day03 implements Day {
     @Override
     public String solvePart1(final List<String> input) {
         final int patternLength = input.get(0).length();
-        return "" + IntStream.range(0, input.size()).filter(i -> input.get(i).charAt((i * 3) % patternLength) == '#').count();
-        //return "0";
+        final long result = IntStream.range(0, input.size())
+                .filter(i -> input.get(i).charAt((i * 3) % patternLength) == '#')
+                .count();
+        return "" + result;
     }
 
     @Override
     public String solvePart2(final List<String> input) {
         final int patternLength = input.get(0).length();
-        // {1, 2}
-        // , {3, 1}, {5, 1}, {7, 1}
+
+        // Slope in {right, down}
         final int[][] slopes = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
-        long result = 1;
-        for (final var slope : slopes) {
-            final int right = slope[0];
-            final int down = slope[1];
-            final long trees = IntStream
-                    .range(0, input.size())
-                    .skip(1)
-                    .filter(i -> (i) % down == 0)
-                    .peek(i -> System.out.println(i + " " + input.get(i)))
-                    .filter(i -> input.get(i).charAt(((i / down) * right) % patternLength) == '#')
-                    .peek(i -> System.out.println(i))
-                    .count();
-            System.out.println(trees);
-            result *= trees;
-        }
+
+        final long result = Arrays.stream(slopes).parallel().mapToLong(
+                slope -> {
+                    final int right = slope[0];
+                    final int down = slope[1];
+                    return IntStream
+                            .range(0, input.size())
+                            .filter(i -> i % down == 0)
+                            .filter(i -> input.get(i).charAt(((i / down) * right) % patternLength) == '#')
+                            .count();
+                }
+        ).reduce(1, (a, b) -> a * b);
         return "" + result;
     }
 }
