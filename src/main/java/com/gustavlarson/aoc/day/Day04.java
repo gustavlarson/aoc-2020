1569e24b5e366d0cb3af5690378ac75b64fa6009
Gustav Larson <gustav.larson@gmail.com>
2020-12-04 07:40:13 +0100

    Solve p1

--- src/main/java/com/gustavlarson/aoc/day/Day04.java
+++ src/main/java/com/gustavlarson/aoc/day/Day04.java
@@ -2,13 +2,40 @@ package com.gustavlarson.aoc.day;
 
 import com.gustavlarson.aoc.Day;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day04 implements Day {
 
     @Override
     public String solvePart1(final List<String> input) {
-        return null;
+        final List<String> passports = new ArrayList<>();
+        var tempString = "";
+        for (final var line : input) {
+            if (line.length() > 0) {
+                tempString = tempString.concat(" " + line);
+            } else {
+                passports.add(tempString);
+                tempString = "";
+            }
+        }
+        passports.add(tempString);
+
+        var counter = 0;
+        for (final var passport : passports) {
+            if (passport.contains("byr:") &&
+                    passport.contains("iyr:") &&
+                    passport.contains("eyr:") &&
+                    passport.contains("hgt:") &&
+                    passport.contains("hcl:") &&
+                    passport.contains("ecl:") &&
+                    passport.contains("pid:")
+            ) {
+                counter++;
+            }
+        }
+
+        return "" + counter;
     }
 
     @Override
--- src/test/java/com/gustavlarson/aoc/day/Day04Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day04Test.java
@@ -10,10 +10,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day04Test {
     @Test
     public void testPart101() {
-        final Day day = new Day01();
-        final List<String> input = List.of("1", "2");
+        final Day day = new Day04();
+        final List<String> input = List.of(
+                "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+                "byr:1937 iyr:2017 cid:147 hgt:183cm", "",
+                "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+                "hcl:#cfa07d byr:1929");
         final String result = day.solvePart1(input);
         assertEquals("1", result);
     }
 
+    @Test
+    public void testPart102() {
+        final Day day = new Day04();
+        final List<String> input = List.of(
+                "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+                "byr:1937 iyr:2017 cid:147 hgt:183cm",
+                "",
+                "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+                "hcl:#cfa07d byr:1929",
+                "hcl:#ae17e1 iyr:2013",
+                "eyr:2024",
+                "ecl:brn pid:760753108 byr:1931",
+                "hgt:179cm",
+                "",
+                "hcl:#cfa07d eyr:2025 pid:166559648",
+                "iyr:2011 ecl:brn hgt:59in");
+        final String result = day.solvePart1(input);
+        assertEquals("2", result);
+    }
 }
