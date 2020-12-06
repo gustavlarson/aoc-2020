c3821f343fca18184c8217c09a8f137eed17da34
Gustav Larson <gustav.larson@gmail.com>
2020-12-06 16:28:50 +0100

    Basic test

--- src/main/java/com/gustavlarson/aoc/day/Day06.java
+++ src/main/java/com/gustavlarson/aoc/day/Day06.java
@@ -2,13 +2,24 @@ package com.gustavlarson.aoc.day;
 
 import com.gustavlarson.aoc.Day;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Day06 implements Day {
 
+    static int getNumberOfAnswers(final String input) {
+        return 3;
+    }
+
     @Override
     public String solvePart1(final List<String> input) {
-        return null;
+        final int res = Arrays
+                .stream(String.join(" \n", input).split("\n \n"))
+                .parallel()
+                .mapToInt(Day06::getNumberOfAnswers)
+                .sum();
+
+        return "" + res;
     }
 
     @Override
--- src/test/java/com/gustavlarson/aoc/day/Day06Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day06Test.java
@@ -10,10 +10,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day06Test {
     @Test
     public void testPart101() {
-        final Day day = new Day01();
-        final List<String> input = List.of("1", "2");
+        final Day day = new Day06();
+        final List<String> input = List.of("abc");
         final String result = day.solvePart1(input);
-        assertEquals("1", result);
+        assertEquals("3", result);
+    }
+
+    @Test
+    public void testPart102() {
+        final Day day = new Day06();
+        final List<String> input = List.of("abc", "", "a", "b", "c");
+        final String result = day.solvePart1(input);
+        assertEquals("6", result);
     }
 
 }
