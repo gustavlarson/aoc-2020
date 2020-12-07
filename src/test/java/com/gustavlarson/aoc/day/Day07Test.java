e7c9e593aa506c9e049c9ec4083e7d8a2dde5b9b
Gustav Larson <gustav.larson@gmail.com>
2020-12-07 08:45:26 +0100

    Solve P1

--- src/test/java/com/gustavlarson/aoc/day/Day07Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day07Test.java
@@ -11,9 +11,36 @@ public class Day07Test {
     @Test
     public void testPart101() {
         final Day day = new Day07();
-        final List<String> input = List.of("1", "2");
+        final List<String> input = List.of("bright white bags contain 1 shiny gold bag.");
         final String result = day.solvePart1(input);
         assertEquals("1", result);
+    }
+
+    @Test
+    public void testPart102() {
+        final Day day = new Day07();
+        final List<String> input = List.of(
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "dotted black bags contain no other bags.");
+        final String result = day.solvePart1(input);
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void testPart103() {
+        final Day day = new Day07();
+        final List<String> input = List.of("light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags.");
+        final String result = day.solvePart1(input);
+        assertEquals("4", result);
     }
 
 }
