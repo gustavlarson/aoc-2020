5634a7bd2bd9bdd1303f98e398ae02fc1137ac43
Gustav Larson <gustav.larson@gmail.com>
2020-12-07 09:08:40 +0100

    Solve Solve P2

--- src/main/java/com/gustavlarson/aoc/day/Day07.java
+++ src/main/java/com/gustavlarson/aoc/day/Day07.java
@@ -12,7 +12,6 @@ public class Day07 implements Day {
     private final Pattern colorPattern = Pattern.compile("(?<name>.*) bags contain (?<rule>.*)");
     private final Pattern rulePattern = Pattern.compile("(?<number>\\d+) (?<name>[\\w\\s]+) bag");
 
-    static Map<String, Map<String, Integer>> rules = new HashMap<>();
 
     class Rule {
         private final String name;
@@ -45,6 +44,8 @@ public class Day07 implements Day {
 
     @Override
     public String solvePart1(final List<String> input) {
+        final Map<String, Map<String, Integer>> rules = new HashMap<>();
+
         input.forEach(line -> {
             final Matcher m = colorPattern.matcher(line);
             if (!m.find()) {
@@ -63,18 +64,47 @@ public class Day07 implements Day {
             rules.put(name, map);
         });
 
-        return "" + rules.keySet().stream().filter(Day07::containsShinyGold).count();
+        return "" + rules.keySet().stream().filter(key -> containsShinyGold(rules, key)).count();
     }
 
-    private static boolean containsShinyGold(final String color) {
+    private static boolean containsShinyGold(final Map<String, Map<String, Integer>> rules, final String color) {
         if (rules.get(color) == null) return false;
         if (rules.get(color).containsKey("shiny gold")) return true;
         if (rules.get(color).isEmpty()) return false;
-        return rules.get(color).keySet().stream().anyMatch(Day07::containsShinyGold);
+        return rules.get(color).keySet().stream().anyMatch(key -> containsShinyGold(rules, key));
     }
 
     @Override
     public String solvePart2(final List<String> input) {
-        return null;
+        final Map<String, Map<String, Integer>> rules = new HashMap<>();
+
+        input.forEach(line -> {
+            final Matcher m = colorPattern.matcher(line);
+            if (!m.find()) {
+                throw new IllegalArgumentException();
+            }
+            final String name = m.group("name");
+            final String rule = m.group("rule");
+
+            final Matcher mr = rulePattern.matcher(rule);
+
+            final Map<String, Integer> map = new HashMap<>();
+
+            while (mr.find()) {
+                map.put(mr.group("name"), Integer.parseInt(mr.group("number")));
+            }
+            rules.put(name, map);
+        });
+
+        return "" + (countBags(rules, "shiny gold") - 1);
+
+    }
+
+    private static int countBags(final Map<String, Map<String, Integer>> rules, final String key) {
+        final int[] count = {0};
+        rules.get(key).forEach((k, v) -> {
+            count[0] += countBags(rules, k) * v;
+        });
+        return count[0] + 1;
     }
 }
--- src/test/java/com/gustavlarson/aoc/day/Day07Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day07Test.java
@@ -43,4 +43,19 @@ public class Day07Test {
         assertEquals("4", result);
     }
 
+    @Test
+    public void testPart201() {
+        final Day day = new Day07();
+        final List<String> input = List.of("light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags.");
+        final String result = day.solvePart2(input);
+        assertEquals("32", result);
+    }
 }
