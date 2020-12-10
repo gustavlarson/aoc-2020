6c7264e740dc93c87868d4b816c1eb7f37d7efdb
Gustav Larson <gustav.larson@gmail.com>
2020-12-10 08:41:32 +0100

    Solve P2

--- src/main/java/com/gustavlarson/aoc/day/Day10.java
+++ src/main/java/com/gustavlarson/aoc/day/Day10.java
@@ -2,6 +2,7 @@ package com.gustavlarson.aoc.day;
 
 import com.gustavlarson.aoc.Day;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,13 +12,13 @@ public class Day10 implements Day {
 
     public Day10(final List<String> input) {
         this.input = input.stream().map(Integer::parseInt).sorted().collect(Collectors.toList());
+        this.input.add(0, 0);
     }
 
     @Override
     public long solvePart1() {
         var one = 0;
         var three = 1; // Start with one
-        input.add(0, 0);
         for (var i = 1; i < input.size(); i++) {
             switch (input.get(i) - input.get(i - 1)) {
                 case 1:
@@ -37,6 +38,28 @@ public class Day10 implements Day {
 
     @Override
     public long solvePart2() {
-        return 0;
+        final long[] dp = new long[input.size()];
+        Arrays.fill(dp, -1);
+        return count(0, dp);
+    }
+
+    private long count(final int index, final long[] dp) {
+        if (index == input.size() - 1) {
+            return 1;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index];
+        } else {
+            long count = 0;
+            for (var i = index + 1; i <= Math.min(index + 3, input.size() - 1); i++) {
+                if (input.get(index) + 3 >= input.get(i)) {
+                    count += count(i, dp);
+                }
+            }
+            dp[index] = count;
+            return count;
+        }
+
     }
 }
--- src/test/java/com/gustavlarson/aoc/day/Day10Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day10Test.java
@@ -9,70 +9,78 @@ import static com.gustavlarson.aoc.day.TestHelper.getAsList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day10Test {
+
+    public static final List<String> INPUT1 = getAsList("""
+            16
+            10
+            15
+            5
+            1
+            11
+            7
+            19
+            6
+            12
+            4""");
+    public static final List<String> INPUT2 = getAsList("""
+            28
+            33
+            18
+            42
+            31
+            14
+            46
+            20
+            48
+            47
+            24
+            23
+            49
+            45
+            19
+            38
+            39
+            11
+            1
+            32
+            25
+            35
+            8
+            17
+            7
+            9
+            4
+            2
+            34
+            10
+            3""");
+
     @Test
     public void testPart101() {
-        final List<String> input = getAsList("""
-                16
-                10
-                15
-                5
-                1
-                11
-                7
-                19
-                6
-                12
-                4""");
+        final List<String> input = INPUT1;
         final Day day = new Day10(input);
         assertEquals(5 * 7, day.solvePart1());
     }
 
     @Test
     public void testPart102() {
-        final List<String> input = getAsList("""
-                28
-                33
-                18
-                42
-                31
-                14
-                46
-                20
-                48
-                47
-                24
-                23
-                49
-                45
-                19
-                38
-                39
-                11
-                1
-                32
-                25
-                35
-                8
-                17
-                7
-                9
-                4
-                2
-                34
-                10
-                3""");
+        final List<String> input = INPUT2;
         final Day day = new Day10(input);
         assertEquals(22 * 10, day.solvePart1());
     }
 
     @Test
     public void testPart201() {
-        final List<String> input = getAsList("""
-                1
-                2
-                """);
+        final List<String> input = INPUT1;
         final Day day = new Day10(input);
-        assertEquals(0, day.solvePart2());
+        assertEquals(8, day.solvePart2());
+    }
+
+    @Test
+    public void testPart202() {
+        final List<String> input = INPUT2;
+        final Day day = new Day10(input);
+        assertEquals(19208, day.solvePart2());
     }
 
 }
