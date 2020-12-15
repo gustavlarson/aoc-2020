ab5b638c2f656ff7b1ffe88f8a4e7de6597d892c
Gustav Larson <gustav.larson@gmail.com>
2020-12-15 07:35:02 +0100

    First attempt

---
+++ src/main/java/com/gustavlarson/aoc/day/Day15.java
@@ -0,0 +1,49 @@
+package com.gustavlarson.aoc.day;
+
+import com.gustavlarson.aoc.Day;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day15 implements Day {
+
+    private final List<Long> input;
+
+    public Day15(final List<String> input) {
+        this.input = Arrays.stream(input.get(0).split(",")).map(Long::parseLong).collect(Collectors.toList());
+    }
+
+    @Override
+    public long solvePart1() {
+        long rounds[] = new long[2020];
+        for (var i = 0; i < input.size(); i++) {
+            rounds[i] = input.get(i);
+        }
+        for (var i = input.size(); i < rounds.length; i++) {
+            var lastNumber = rounds[i - 1];
+            long nextNumber;
+            if (Arrays.stream(rounds).filter(num -> num == lastNumber).count() == 1) {
+                nextNumber = 0;
+            } else {
+                long lastOccurrance = lastOccurrance(rounds, lastNumber, i);
+                nextNumber = (i - 1) - lastOccurrance;
+            }
+
+            rounds[i] = nextNumber;
+        }
+        return rounds[rounds.length - 1];
+    }
+
+    private static long lastOccurrance(long[] rounds, long lastNumber, int i) {
+        for (var j = i - 2; j >= 0; j--) {
+            if (rounds[j] == lastNumber) return j;
+        }
+        throw new IllegalStateException();
+    }
+
+    @Override
+    public long solvePart2() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/gustavlarson/aoc/day/Day15Test.java
@@ -0,0 +1,36 @@
+package com.gustavlarson.aoc.day;
+
+import com.gustavlarson.aoc.Day;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.gustavlarson.aoc.day.TestHelper.getAsList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day15Test {
+    @Test
+    public void testPart101() {
+        final List<String> input = List.of("0,3,6");
+        final Day day = new Day15(input);
+        assertEquals(436, day.solvePart1());
+    }
+
+    @Test
+    public void testPart102() {
+        final List<String> input = List.of("1,3,2");
+        final Day day = new Day15(input);
+        assertEquals(1, day.solvePart1());
+    }
+
+    @Test
+    public void testPart201() {
+        final List<String> input = getAsList("""
+                1
+                2
+                """);
+        final Day day = new Day15(input);
+        assertEquals(0, day.solvePart2());
+    }
+
+}
