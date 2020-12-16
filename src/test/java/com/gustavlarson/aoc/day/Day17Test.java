75e5a9ced3f4a59ff6c169edae1a7f9ad8d73bf5
Gustav Larson <gustav.larson@gmail.com>
2020-12-16 16:01:08 +0100

    Update test template

--- src/test/java/com/gustavlarson/aoc/day/Day17Test.java
+++ src/test/java/com/gustavlarson/aoc/day/Day17Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static com.gustavlarson.aoc.day.TestHelper.getAsList;
+import static com.gustavlarson.aoc.day.TestHelper.getFromFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day17Test {
@@ -26,4 +27,17 @@ public class Day17Test {
         assertEquals(0, day.solvePart2());
     }
 
+    @Test
+    public void testPart1Real() {
+        final List<String> input = getFromFile(17);
+        final Day day = new Day17(input);
+        assertEquals(0, day.solvePart1());
+    }
+
+    @Test
+    public void testPart2Real() {
+        final List<String> input = getFromFile(17);
+        final Day day = new Day17(input);
+        assertEquals(0, day.solvePart2());
+    }
 }
